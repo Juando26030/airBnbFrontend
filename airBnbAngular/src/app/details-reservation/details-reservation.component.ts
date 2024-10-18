8b8a85c288b03173f2660ef91fbed2ce6c26a706
sage <antoshkakorol301@gmail.com>
2024-10-18 12:16:02 -0500

    informacion de reserva html inicial

--- airBnbAngular/src/app/details-reservation/details-reservation.component.ts
+++ airBnbAngular/src/app/details-reservation/details-reservation.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "../components/login/login.component";
-import { MenuComponent } from "../components/menu/menu.component";
-import { CardPropiedadComponent } from "../components/card-propiedad/card-propiedad.component";
-import { PropiedadComponent } from "../components/propiedad/propiedad.component";
-import { FooterComponent } from "../components/footer/footer.component";
+import { DropdownModule } from 'primeng/dropdown';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule }
+    from "@angular/platform-browser/animations";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, MenuComponent, CardPropiedadComponent, PropiedadComponent, FooterComponent],
+  imports: [DropdownModule],
   templateUrl: './details-reservation.component.html',
   styleUrls: ['./details-reservation.component.css']
 })
 export class DetailsReservationComponent {
+  paymentMethods = [
+    { name: "Tarjeta" },
+    { name: "Transferencia Bancaria" },
+  ];
 
 }
